19628304eb767eda4e2f1fa44f5be7f4396afc8b
rameshcompany <78635795+rameshcompany@users.noreply.github.com>
2021-06-18 12:00:33 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-yamlcount=$(find /tmp/*.yaml | wc -l)
-echo "yaml file count is: $yamlcount"
-if [ $yamlcount -gt 8 ];then
+yamlfilecount=$(find /tmp/*.yaml | wc -l)
+echo "yaml file count is: $yamlfilecount"
+if [ $yamlfilecount -gt 8 ];then
 
    echo "yaml files are greter than 8"
    sudo tar -zcvf /yaml.tar /tmp/*.yaml
    
-else [ $yamlcount -lt 8 ];then
+else [ $yamlfilecount -lt 8 ];then
 
       sudo rm -rf /tmp/*.yaml
-      jsonfile=$(find /tmp/*.json | wc -l)
+      jsonfilecount=$(find /tmp/*.json | wc -l)
       
 fi
 
-if [ $yamlcount -lt 8 && $jsonfile -lt 10];then
+if [ $yamlfilecount -lt 8 && $jsonfilecount -lt 10];then
     sudo mv /tmp/*.json /tmp/*.yaml
 fi
    
